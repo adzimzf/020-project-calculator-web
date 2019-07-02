0be6e99d733f4b8a76ed75b061f862b32700c3cf
Adzim Zul Fahmi <adzimzulfahmy@gmail.com>
2019-07-02 22:11:38 +0700

    feature multiple

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let resetScreen = false;
 let mayCount = false;
 let temp = '';
 let operator = '';
-button.addEventListener('click', function (e) {
+button.addEventListener('click', function(e) {
     let buttonClick = e.target;
     let valueButton = buttonClick.innerText;
     if (valueButton == 'C') {
@@ -12,30 +12,69 @@ button.addEventListener('click', function (e) {
     } else if (valueButton == '<') {
         screen.value = screen.value.slice(0, -1);
     } else if (valueButton == '=') {
-        if (mayCount == true) {
-            screen.value = eval(temp + operator + screen.value);
-            mayCount = false;
-        }
-    }
-    else if (buttonClick.classList.contains('operator')) {
-        if (mayCount == true) {
-            screen.value = eval(temp + operator + screen.value);
-            mayCount = false;
-        }
-        temp = screen.value;
-        operator = valueButton;
-        resetScreen = true;
-    }
-    else {
-        if (resetScreen == true) {
-            screen.value = valueButton;
-            resetScreen = false;
-            mayCount = true;
-        } else {
-            screen.value = screen.value + valueButton;
-        }
+        screen.value = hitung(screen.value);
+    } else if (buttonClick.classList.contains('operator')) {
+        screen.value = screen.value + valueButton;
+    } else {
+
+        screen.value = screen.value + valueButton;
+
     }
 });
 
+function hitung(str) {
+    if (str.includes('*')) {
+        a = parseFloat(getAwal(str, '*'))
+        k = parseFloat(getAkhir(str, '*'))
+        h = a * k
+        return hitung(str.replace(a.toString() + "*" + k.toString(), h.toString()))
+    }
+    if (str.includes('/')) {
+        a = parseFloat(getAwal(str, '/'))
+        k = parseFloat(getAkhir(str, '/'))
+        h = a / k
+        return hitung(str.replace(a.toString() + "/" + k.toString(), h.toString()))
+    }
+    if (str.includes('+')) {
+        a = parseFloat(getAwal(str, '+'))
+        k = parseFloat(getAkhir(str, '+'))
+        h = a + k
+        return hitung(str.replace(a.toString() + "+" + k.toString(), h.toString()))
+    }
+    if (str.includes('-')) {
+        a = parseFloat(getAwal(str, '-'))
+        k = parseFloat(getAkhir(str, '-'))
+        h = a - k
+        return hitung(str.replace(a.toString() + "-" + k.toString(), h.toString()))
+    }
+    return str
+}
 
+function getAwal(str, delemiter) {
+    var awal = str.split(delemiter)[0].split('');
+    let aawal = 0;
+    for (var i = awal.length - 1; i >= 0; i--) {
 
+        if (awal[i] == "*" || awal[i] == "/" || awal[i] == "+" || awal[i] == "-") {
+            aawal = str.split(delemiter)[0].substring(i + 1, awal.length);
+        }
+    }
+    if (aawal == 0) {
+        return str.split(delemiter)[0]
+    }
+    return aawal;
+}
+
+function getAkhir(str, delemiter) {
+    var awal = str.split(delemiter)[1].split('');
+    let aawal = 0;
+    for (var i = 0; i < awal.lenth; i++) {
+        if (awal[i] == "*" || awal[i] == "/" || awal[i] == "+" || awal[i] == "-") {
+            aawal = str.split(delemiter)[1].substring(i, awal.length);
+        }
+    }
+    if (aawal == 0) {
+        return str.split(delemiter)[1]
+    }
+    return aawal;
+}
